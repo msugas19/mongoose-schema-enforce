00a302bbe6027afb0252903a2c1b0b7ffc184c3b
msugas19 <msugas19@gmail.com>
2017-06-14 14:00:42 -0700

    Added nested Array support
    Added support for nested arrays defined in mongoose schemas as well as exposed internal method for schema validation to be utilized for more complex queries.

--- app.js
+++ app.js
@@ -5,7 +5,10 @@
 'use strict';
 
 
-module.exports = findSchemaSafePlugin;
+module.exports = {
+    findSchemaSafePlugin : findSchemaSafePlugin,
+    enforceSchema : iterateThroughSchema
+};
 
 function findSchemaSafePlugin(schema) {
     schema.statics.findSchemaSafe = function findSchemaSafe(condition, options, callback) {
@@ -26,11 +29,13 @@ function findSchemaSafePlugin(schema) {
 }
 
 function iterateThroughSchema(schema, results, options) {
-    var tasks = [];
+
+    if(results === null ){return {};}
 
     results.forEach(function (element, index, array) {
         array[index] = iterateNestedObjects(schema, element, options);
     });
+
 
     return results;
 }
@@ -41,15 +46,22 @@ function iterateNestedObjects(schema, result, options) {
 
     Object.keys(schema.paths).forEach(function (key, ind, arr) {
         if (schema.paths[key].hasOwnProperty("schema")) {
-            element[key] = iterateNestedObjects(schema.paths[key].schema, result[key], options);
-        } else {    
+            if(schema.paths[key].instance === "Array"){
+                //Handle nested Arrays
+                element[key] = iterateThroughSchema(schema.paths[key].schema, result[key], options);
+            } else {
+                //Handle nested objects
+                element[key] = iterateNestedObjects(schema.paths[key].schema, result[key], options);
+            }
+        } else {
             element[key] = result[key] ? result[key] : null;
         }
     });
 
-    if (options.removeFields !== null) {
+    if (options !== undefined && options.removeFields !== undefined && options.removeFields !== null) {
         //Remove specified fields
         try {
+
             options.removeFields.forEach(function (field) {
                 try {
                     delete element[field];
